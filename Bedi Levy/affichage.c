a49bcc249c3e6c97b37188c1264e068fc99605b1
Levy2000 <47690269+Levy2000@users.noreply.github.com>
2019-04-13 15:38:50 +0000

    Add files via upload

---
+++ Bedi Levy/affichage.c
@@ -0,0 +1,77 @@
+﻿#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <SDL/SDL.h>
+
+#include <SDL/SDL_image.h> /* Inclusion du header de SDL_image (adapter le dossier au besoin) */
+
+ 
+void pause();
+
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL, *imageDeFond = NULL, *jeton = NULL;
+    SDL_Rect positionFond, positioncoin;
+
+    positionFond.x = 0;
+    positionFond.y = 0;
+    positionjeton.x = 270;
+    positionjeton.y = 111;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WM_SetIcon(SDL_LoadBMP("background.bmp"), NULL);
+
+    ecran = SDL_SetVideoMode(1600, 400, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Chargement d'images en SDL", NULL);
+
+    imageDeFond = SDL_LoadBMP("background.bmp");
+    SDL_BlitSurface(imageDeFond, NULL, ecran, &positionFond);
+
+    /* Chargement et blittage de bar sur la scène */
+    jeton = SDL_LoadBMP("jeton.jpg");
+    SDL_BlitSurface(jeton, NULL, ecran, &positionjeton);
+
+    SDL_Flip(ecran);
+    
+
+/* On charge l'image : */
+jeton = IMG_Load("jeton.jpg");
+SDL_SetColorKey(jeton, SDL_SRCCOLORKEY, SDL_MapRGB(jeton->format, 0, 0, 0));
+/* On blitte l'image maintenant transparente sur le fond : */
+SDL_BlitSurface(jeton, NULL, ecran, &positionjeton);
+SDL_Flip(ecran);
+
+void init_coin(coin* c)
+{
+    c->img[0] = IMG_Load("1.png");
+    c->img[1] = IMG_Load("2.png");
+    c->img[2] = IMG_Load("3.png");
+    c->img[3] = IMG_Load("4.png");
+    c->valeur=10;
+}
+    SDL_FreeSurface(imageDeFond);
+    SDL_FreeSurface(jeton);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
+ 
+void pause()
+{
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
+
+
---
+++ Bedi Levy/animation.h
@@ -0,0 +1,23 @@
+#include <SDL/SDL.h>
+
+#include<SDL/SDL_image.h>
+
+#include <SDL/SDL_mixer.h>
+#ifndef DEF_ANIMATION
+#define DEF_ANIMATION
+
+int main (int argc , char *argv[]);
+SDL_Init(SDL_INIT_VIDEO); 
+SDL_WM_Setcaption("SDL Move","SDL Move");
+SDL_SetVideoMode(SCREEN_WIDTH,SCREEN_HEIGHT,0,0);
+SDL_SetColorKey(sprite,SDL_SRCCOLORKEY | SDL_RLEACCEL, colorkey);
+SDL_PollEvent(&event));
+SDL_KEYDOWN;
+SDL_GetKeyState(NULL);
+SDL_BlitSurface(sprite,NULL,screen,&rcGrass);
+
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+#define SPRITE_SIZE    32
+
+
